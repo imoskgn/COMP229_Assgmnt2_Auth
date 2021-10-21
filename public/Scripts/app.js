b944717c2a9469740b55c3f1774a3d57696ad641
Oscar Gonzalez <imoskgn@gmail.com>
2021-10-21 01:03:07 -0400

    First commit

---
+++ public/Scripts/app.js
@@ -0,0 +1,14 @@
+/*  File Name: app.js
+    Student: Oscar Eduardo Gonzalez-Neira 
+    Code: 301185878
+    ---------------------------------------------
+    Date            Update
+    03/10/2021  Initial implementation
+*/
+// IIFE Immediately Invoked Function Expression
+(function(){
+    function Start(){
+        console.log("App started...")
+    }
+    window.addEventListener("load", Start)
+})
