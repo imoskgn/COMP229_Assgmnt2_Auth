959d56b0687040a11780c752d941f6800cc06d22
Oscar Gonzalez <imoskgn@gmail.com>
2021-10-21 21:17:54 -0400

    Views for editing

---
+++ server/views/contact/add.js
@@ -0,0 +1,7 @@
+<!-- Including header and main navigation panel partials -->
+<%- include('../partials/header.ejs') %> <%- include('../partials/main_nav.ejs')%>
+
+
+
+<!-- Including footer and bottom panel partials -->
+<%- include('../partials/bottom_nav.ejs')%> <%- include('../partials/footer.ejs') %>
